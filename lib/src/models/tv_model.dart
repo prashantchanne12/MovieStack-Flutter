d5ea25971b433dea9d0dd22785cd09bb4dc567ef
prashantchanne12 <prashantchanne121@gmail.com>
2020-07-14 10:45:42 +0530

    cleaned code and created movie details model

--- lib/src/models/tv_model.dart
+++ lib/src/models/tv_model.dart
@@ -2,14 +2,10 @@ class TvModel {
   String original_name;
   int id;
   String name;
-  int vote_count;
   double vote_average;
-  String first_air_date;
   String poster_path;
-  List<dynamic> genre_ids;
   String original_language;
   String backdrop_path;
-  String overview;
   double popularity;
   String media_type;
 
@@ -18,14 +14,10 @@ class TvModel {
     original_name = parsedJson['original_name'];
     id = parsedJson['id'];
     name = parsedJson['name'];
-    vote_count = parsedJson['vote_count'];
     vote_average = parsedJson['vote_average'];
-    first_air_date = parsedJson['first_air_date'];
     poster_path = parsedJson['poster_path'];
-    genre_ids = parsedJson['genre_ids'];
     original_language = parsedJson['original_language'];
     backdrop_path = parsedJson['backdrop_path'];
-    overview = parsedJson['overview'];
     popularity = parsedJson['popularity'];
     media_type = parsedJson['media_type'];
   }
