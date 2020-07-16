e04b88851e5c3f4eebad8951309d16d99daf6ad4
prashantchanne12 <prashantchanne121@gmail.com>
2020-07-16 13:40:48 +0530

    added tap to details page on search screen

--- lib/src/screens/search.dart
+++ lib/src/screens/search.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:movie_stack/src/blocs/details_page_provider.dart';
 import 'package:movie_stack/src/blocs/home_bloc.dart';
 import 'package:movie_stack/src/blocs/home_provider.dart';
 import 'package:movie_stack/src/constants.dart';
 import 'package:movie_stack/src/models/trending_model.dart';
+import 'package:movie_stack/src/screens/details_page.dart';
 
 class Search extends StatelessWidget {
   final TextEditingController searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     HomeBloc homeBloc = HomeProvider.of(context);
+    DetailsBloc detailsBloc = DetailsPageProvider.of(context);
     return Scaffold(
       backgroundColor: kPrimaryColor,
       appBar: AppBar(
@@ -40,16 +43,16 @@ class Search extends StatelessWidget {
                 Icons.clear,
                 color: Colors.white,
               ),
-              onPressed: clearSearch,
+              onPressed: () => clearSearch(context),
             ),
           ),
         ),
       ),
-      body: buildSearchResults(homeBloc),
+      body: buildSearchResults(homeBloc, detailsBloc),
     );
   }
 
-  buildSearchResults(HomeBloc homeBloc) {
+  buildSearchResults(HomeBloc homeBloc, DetailsBloc detailsBloc) {
     return StreamBuilder(
       stream: homeBloc.search,
       builder: (BuildContext context, snapshot) {
@@ -104,108 +107,133 @@ class Search extends StatelessWidget {
             bool isMovieOrTv = snapshot.data[index]['media_type'] == 'movie' ||
                 snapshot.data[index]['media_type'] == 'tv';
             return isMovieOrTv
-                ? Padding(
-                    padding: const EdgeInsets.only(left: 10.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Container(
-                              height: 150.0,
-                              child: ClipRRect(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(5.0)),
-                                child: FadeInImage.assetNetwork(
-                                  image: snapshot.data[index]['poster_path']
-                                              .toString()
-                                              .length >
-                                          0
-                                      ? '$kImageUrl${snapshot.data[index]['poster_path']}'
-                                      : '$kImageBlueUrl',
-                                  fit: BoxFit.fill,
-                                  placeholder: 'assets/images/img.png',
+                ? GestureDetector(
+                    onTap: () {
+                      openDetailsScreen(context, snapshot.data[index]['id'],
+                          isMovie, detailsBloc);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Container(
+                                height: 150.0,
+                                width: 100.0,
+                                child: ClipRRect(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5.0)),
+                                  child: FadeInImage.assetNetwork(
+                                    image: snapshot.data[index]
+                                                ['poster_path'] !=
+                                            null
+                                        ? '$kImageUrl${snapshot.data[index]['poster_path']}'
+                                        : '$kImageBlueUrl',
+                                    fit: BoxFit.fill,
+                                    placeholder: 'assets/images/img.png',
+                                  ),
                                 ),
                               ),
-                            ),
-                            Container(
-                              alignment: Alignment.centerLeft,
-                              padding: EdgeInsets.only(left: 20.0),
-                              height: 150.0,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Row(
-                                    children: <Widget>[
-                                      Container(
-                                        child: Text(
-                                          isMovie
-                                              ? '${snapshot.data[index]['title'].toString().length < 15 ? snapshot.data[index]['title'] : snapshot.data[index]['title'].toString().substring(0, 15) + '..'}'
-                                              : '${snapshot.data[index]['name']}',
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 18.0,
-                                          ),
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                      ),
-                                      Container(
-                                        child: Text(
-                                          isMovie
-                                              ? ' (${snapshot.data[index]['release_date'].toString().substring(0, 4)})'
-                                              : ' (${snapshot.data[index]['first_air_date'].toString().substring(0, 4)})',
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 18.0,
-                                          ),
-                                          textAlign: TextAlign.left,
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  SizedBox(
-                                    height: 5.0,
-                                  ),
-                                  Row(
-                                    children: <Widget>[
-                                      Icon(
-                                        Icons.stars,
-                                        color: kAccentColor,
-                                      ),
-                                      SizedBox(
-                                        width: 5.0,
-                                      ),
-                                      Container(
-                                        child: Text(
-                                          '${snapshot.data[index]['vote_average']}',
-                                          style: TextStyle(
-                                            color: Colors.white,
+                              Container(
+                                alignment: Alignment.centerLeft,
+                                padding: EdgeInsets.only(left: 20.0),
+                                height: 150.0,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Row(
+                                      children: <Widget>[
+                                        Container(
+                                          child: Text(
+                                            isMovie
+                                                ? '${snapshot.data[index]['title'].toString().length < 15 ? snapshot.data[index]['title'] : snapshot.data[index]['title'].toString().substring(0, 15) + '..'}'
+                                                : '${snapshot.data[index]['name']}',
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 18.0,
+                                            ),
+                                            overflow: TextOverflow.ellipsis,
                                           ),
                                         ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
+                                        Container(
+                                          child: Text(
+                                            isMovie
+                                                ? ' (${snapshot.data[index]['release_date'].toString().substring(0, 4)})'
+                                                : ' (${snapshot.data[index]['first_air_date'].toString().substring(0, 4)})',
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 18.0,
+                                            ),
+                                            textAlign: TextAlign.left,
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    Row(
+                                      children: <Widget>[
+                                        Icon(
+                                          Icons.stars,
+                                          color: kAccentColor,
+                                        ),
+                                        SizedBox(
+                                          width: 5.0,
+                                        ),
+                                        Container(
+                                          child: Text(
+                                            '${snapshot.data[index]['vote_average']}',
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                        Divider(),
-                      ],
+                            ],
+                          ),
+                          Divider(),
+                        ],
+                      ),
                     ),
                   )
-                : Text('');
+                : Container(
+                    height: 0.0,
+                  );
           },
         );
       },
     );
   }
 
-  clearSearch() {
+  openDetailsScreen(
+      BuildContext context, int id, bool isMovie, DetailsBloc detailsBloc) {
+    detailsBloc.fetchDetails(id, isMovie ? 'movie' : 'tv');
+    detailsBloc.fetchCast(id, isMovie ? 'movie' : 'tv');
+    detailsBloc.fetchReviews(id);
+    detailsBloc.fetchSimilar(id, isMovie ? 'movie' : 'tv');
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => DetailsPage(
+          isMovie: isMovie,
+        ),
+      ),
+    );
+  }
+
+  clearSearch(BuildContext context) {
     searchController.clear();
+    Navigator.pop(context);
   }
 }
